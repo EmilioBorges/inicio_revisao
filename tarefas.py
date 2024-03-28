7807e36f720f80c1f321413bfb2341d2f36be6ca
EmilioBorges <emilio.borges2012@gmail.com>
2024-03-28 20:51:26 -0300

    Funcao criada para somar numeros

---
+++ tarefas.py
@@ -0,0 +1,10 @@
+def somar_numeros(numA, numB):
+    soma = numA + numB
+    return soma
+
+resultado_da_soma = somar_numeros(5,15)
+
+def exibir_soma_resultados(resultado):
+    print(resultado)
+
+exibir_soma_resultados(resultado_da_soma)
