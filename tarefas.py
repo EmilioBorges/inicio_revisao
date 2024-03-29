3fce1d7a0d89520596a55f196d7a5c769a2642e8
EmilioBorges <emilio.borges2012@gmail.com>
2024-03-28 21:03:30 -0300

    Funcao que multiplica 2 numeros

--- tarefas.py
+++ tarefas.py
@@ -1,10 +1,23 @@
+#Funcao para somar dois numeros
+
 def somar_numeros(numA, numB):
     soma = numA + numB
     return soma
 
 resultado_da_soma = somar_numeros(5,15)
 
+#Funcao para multiplicar dois numeros
+
+def multiplicar_numeros(numA, numB):
+    multiplica = numA * numB
+    return multiplica
+
+resultado_da_multiplicacao = multiplicar_numeros(5, 5)
+
+#Funcao para imprimir o resultado das funções de soma e multiplicação
+
 def exibir_soma_resultados(resultado):
-    print(resultado)
+    print(f' {resultado}')
 
 exibir_soma_resultados(resultado_da_soma)
+exibir_soma_resultados(resultado_da_multiplicacao)
