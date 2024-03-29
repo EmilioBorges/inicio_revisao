7ee788bfa20f7e7088bbf512bd94f21080dd7503
EmilioBorges <emilio.borges2012@gmail.com>
2024-03-29 00:45:06 -0300

    Melhorando o print da informações

--- tarefas.py
+++ tarefas.py
@@ -1,10 +1,14 @@
+#Variaveis
+num_a = 5
+num_b = 6
+
 #Funcao para somar dois numeros
 
 def somar_numeros(numA, numB):
     soma = numA + numB
     return soma
 
-resultado_da_soma = somar_numeros(5,15)
+resultado_da_soma = somar_numeros(num_a,num_b)
 
 #Funcao para multiplicar dois numeros
 
@@ -12,12 +16,12 @@ def multiplicar_numeros(numA, numB):
     multiplica = numA * numB
     return multiplica
 
-resultado_da_multiplicacao = multiplicar_numeros(5, 5)
+resultado_da_multiplicacao = multiplicar_numeros(num_a, num_b)
 
 #Funcao para imprimir o resultado das funções de soma e multiplicação
 
 def exibir_resultados(resultado):
-    print(f' {resultado}')
+    print(resultado)
 
-exibir_resultados(f'soma {resultado_da_soma}')
-exibir_resultados(f'multiplicao {resultado_da_multiplicacao}')
+exibir_resultados(f'soma: {num_a} + {num_b} = {resultado_da_soma}')
+exibir_resultados(f'multiplicao: {num_a} x {num_b} = {resultado_da_multiplicacao}')
